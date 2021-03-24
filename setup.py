6fa76135706cdfc88d99c0409cbd9dfa21ad7e04
ppotatoo <rkawas731@outlook.com>
2021-03-24 17:28:48 -0400

    stuffs IG

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+description = 'Asynchronous wrapper for Zane API.'
+
+with open("README.md", 'r') as readme:
+    long_description = readme.read()
+
+version = "0.0.1"
+
+setup(
+    name='asynczane',
+    version=version,
+    author='ppotatoo',
+    description=description,
+    long_description=long_description,
+    packages=['asynczane'],
+    install_requires=['aiohttp'],
+)
+
+
+
